9015187fc56a484bd09f9b8033049db231995d6b
Kennedy <ohirikennedy@yahoo.com>
2021-12-10 09:54:44 -0600

    current location done

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,28 @@
 let lat;
 let lon;
+const currentWeather = document.querySelector('.current-conditions');
+// const currentWeather1 = document.getElementById('current');
+// console.log(theWeather);
+// console.log(currentWeather1);
+
 
 const getWeatherLocation = (url) => {
-  fetch(url).then(response => {
+  return fetch(url).then(response => {
     if (response.ok) {
       return response.json();
     } else {
       Promise.reject();
     }
-  }).then((weatherData) => {
-    console.log(weatherData);
+  })
+}
+
+const getWeeklyForecast = (url) => {
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      Promise.reject();
+    }
   })
 }
 
@@ -20,6 +33,17 @@ if(!navigator.geolocation){
     lat = position.coords.latitude
     lon = position.coords.longitude
     console.log(lat, lon);
-    getWeatherLocation(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=45db684b0d24f95132a33d44dbd10e15`)
+    getWeatherLocation(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=45db684b0d24f95132a33d44dbd10e15`).then((weatherData) => {
+      // let deg = `${(weatherData.main.temp - 32) * 5/9}`;
+      currentWeather.insertAdjacentHTML('afterbegin', `
+      <h2>Current Conditions</h2>
+      <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png" />
+      <div class="current">
+        <div class="temp">${weatherData.main.temp}℃</div>
+        <div class="condition">${weatherData.weather[0].description}</div>
+      </div>
+      `)
+      console.log(weatherData);
+    })
   })
 }
