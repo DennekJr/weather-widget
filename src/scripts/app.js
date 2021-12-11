2ed666c97a2479ba051a391d5bb7f61ecb504d1b
DennekJr <54910517+DennekJr@users.noreply.github.com>
2021-12-11 08:12:03 -0600

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,9 +2,6 @@ let lat;
 let lon;
 const currentWeather = document.querySelector('.current-conditions');
 const weeklyForecast = document.querySelector('.forecast');
-// const currentWeather1 = document.getElementById('current');
-// console.log(theWeather);
-// console.log(currentWeather1);
 
 
 const getWeatherLocation = (url) => {
@@ -33,9 +30,7 @@ if(!navigator.geolocation){
   navigator.geolocation.getCurrentPosition((position) => {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    console.log(lat, lon);
     getWeatherLocation(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=45db684b0d24f95132a33d44dbd10e15`).then((weatherData) => {
-      // let deg = `${(weatherData.main.temp - 32) * 5/9}`;
       currentWeather.insertAdjacentHTML('afterbegin', `
       <h2>Current Conditions</h2>
       <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png" />
@@ -44,14 +39,11 @@ if(!navigator.geolocation){
         <div class="condition">${weatherData.weather[0].description}</div>
       </div>
       `)
-      console.log(weatherData);
     })
 
     getWeeklyForecast(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=45db684b0d24f95132a33d44dbd10e15`).then((forecast) => {
-      console.log(forecast.list);
 
       const sixPm = forecast.list.filter (f => f.dt_txt.split(" ")[1] === "18:00:00")
-      console.log(sixPm)
       sixPm.forEach(day => {
         let date = new Date(day.dt_txt)
         let days = date.toLocaleString("en", { weekday: "long" });
@@ -65,7 +57,6 @@ if(!navigator.geolocation){
         </div>
       </div>
         `)
-        console.log(date.toLocaleString("en", { weekday: "long" }))
       })
 
       // if()
